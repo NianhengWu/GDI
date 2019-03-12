9c629800b59e13333797db1fd268d6462982fe63
nianheng <nianheng.wu@student.uni-tuebingen.de>
2019-03-12 11:05:25 +0100

    change

--- svm_phrase/fusion_methods.py
+++ svm_phrase/fusion_methods.py
@@ -35,7 +35,6 @@ def mean_probability_rule_fusion(result, test_path, n):
     result_matrix = np.true_divide(sentences_m_prob, n)
     for each_row in result_matrix:
         max_num = max(each_row)
-        print(each_row)
         index = list(each_row).index(max_num)
         if index == 0:
             result_label.append('BE')
